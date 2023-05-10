6d94c3d40e4c3f85029695e444324c1395fdb417
Burtininkas <gustasklisauskas@gmail.com>
2023-05-10 14:11:42 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Trading Bot Demo
+
+## Technology Stack & Tools
+
+- Javascript (Receiving info)
+- [Ethers.js](https://docs.ethers.io/v5/) (Blockchain Interaction)
+- [Alchemy](https://www.alchemy.com/) (Blockchain Connection)
+
+## Requirements For Initial Setup
+
+- Install [NodeJS](https://nodejs.org/en/). We recommend using the latest LTS (Long-Term-Support) version, and preferably installing NodeJS via [NVM](https://github.com/nvm-sh/nvm#intro).
+- Create an [Alchemy](https://www.alchemy.com/) account, you'll need to create an app for the Ethereum chain, on the mainnet network
+
+## Setting Up
+
+### 1. Clone/Download the Repository
+
+### 2. Install Dependencies:
+
+`npm install`
+
+### 3. Create and Setup .env
+
+Before running any scripts, you'll want to create a .env file with the following values (see .env.example):
+
+- **ALCHEMY_API_KEY=""**
+
+### 4. Start listener:
+
+In your terminal run:
+`node listener.js`
---
+++ listener.js
@@ -0,0 +1,463 @@
+require("dotenv").config();
+const { ethers } = require("ethers");
+const { Interface } = require("@ethersproject/abi");
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const UNISWAP_V2_ROUTER_ADDR = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+const UNISWAP_V2_ROUTER_ABI = [
+  {
+    inputs: [
+      { internalType: "address", name: "_factory", type: "address" },
+      { internalType: "address", name: "_WETH", type: "address" },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "WETH",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "tokenA", type: "address" },
+      { internalType: "address", name: "tokenB", type: "address" },
+      { internalType: "uint256", name: "amountADesired", type: "uint256" },
+      { internalType: "uint256", name: "amountBDesired", type: "uint256" },
+      { internalType: "uint256", name: "amountAMin", type: "uint256" },
+      { internalType: "uint256", name: "amountBMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "addLiquidity",
+    outputs: [
+      { internalType: "uint256", name: "amountA", type: "uint256" },
+      { internalType: "uint256", name: "amountB", type: "uint256" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "token", type: "address" },
+      { internalType: "uint256", name: "amountTokenDesired", type: "uint256" },
+      { internalType: "uint256", name: "amountTokenMin", type: "uint256" },
+      { internalType: "uint256", name: "amountETHMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "addLiquidityETH",
+    outputs: [
+      { internalType: "uint256", name: "amountToken", type: "uint256" },
+      { internalType: "uint256", name: "amountETH", type: "uint256" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "factory",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountOut", type: "uint256" },
+      { internalType: "uint256", name: "reserveIn", type: "uint256" },
+      { internalType: "uint256", name: "reserveOut", type: "uint256" },
+    ],
+    name: "getAmountIn",
+    outputs: [{ internalType: "uint256", name: "amountIn", type: "uint256" }],
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint256", name: "reserveIn", type: "uint256" },
+      { internalType: "uint256", name: "reserveOut", type: "uint256" },
+    ],
+    name: "getAmountOut",
+    outputs: [{ internalType: "uint256", name: "amountOut", type: "uint256" }],
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountOut", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+    ],
+    name: "getAmountsIn",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+    ],
+    name: "getAmountsOut",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountA", type: "uint256" },
+      { internalType: "uint256", name: "reserveA", type: "uint256" },
+      { internalType: "uint256", name: "reserveB", type: "uint256" },
+    ],
+    name: "quote",
+    outputs: [{ internalType: "uint256", name: "amountB", type: "uint256" }],
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "tokenA", type: "address" },
+      { internalType: "address", name: "tokenB", type: "address" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+      { internalType: "uint256", name: "amountAMin", type: "uint256" },
+      { internalType: "uint256", name: "amountBMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "removeLiquidity",
+    outputs: [
+      { internalType: "uint256", name: "amountA", type: "uint256" },
+      { internalType: "uint256", name: "amountB", type: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "token", type: "address" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+      { internalType: "uint256", name: "amountTokenMin", type: "uint256" },
+      { internalType: "uint256", name: "amountETHMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "removeLiquidityETH",
+    outputs: [
+      { internalType: "uint256", name: "amountToken", type: "uint256" },
+      { internalType: "uint256", name: "amountETH", type: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "token", type: "address" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+      { internalType: "uint256", name: "amountTokenMin", type: "uint256" },
+      { internalType: "uint256", name: "amountETHMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "removeLiquidityETHSupportingFeeOnTransferTokens",
+    outputs: [{ internalType: "uint256", name: "amountETH", type: "uint256" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "token", type: "address" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+      { internalType: "uint256", name: "amountTokenMin", type: "uint256" },
+      { internalType: "uint256", name: "amountETHMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+      { internalType: "bool", name: "approveMax", type: "bool" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "removeLiquidityETHWithPermit",
+    outputs: [
+      { internalType: "uint256", name: "amountToken", type: "uint256" },
+      { internalType: "uint256", name: "amountETH", type: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "token", type: "address" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+      { internalType: "uint256", name: "amountTokenMin", type: "uint256" },
+      { internalType: "uint256", name: "amountETHMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+      { internalType: "bool", name: "approveMax", type: "bool" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "removeLiquidityETHWithPermitSupportingFeeOnTransferTokens",
+    outputs: [{ internalType: "uint256", name: "amountETH", type: "uint256" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "tokenA", type: "address" },
+      { internalType: "address", name: "tokenB", type: "address" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+      { internalType: "uint256", name: "amountAMin", type: "uint256" },
+      { internalType: "uint256", name: "amountBMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+      { internalType: "bool", name: "approveMax", type: "bool" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "removeLiquidityWithPermit",
+    outputs: [
+      { internalType: "uint256", name: "amountA", type: "uint256" },
+      { internalType: "uint256", name: "amountB", type: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountOut", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapETHForExactTokens",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapExactETHForTokens",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapExactETHForTokensSupportingFeeOnTransferTokens",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapExactTokensForETH",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapExactTokensForETHSupportingFeeOnTransferTokens",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapExactTokensForTokens",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapExactTokensForTokensSupportingFeeOnTransferTokens",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountOut", type: "uint256" },
+      { internalType: "uint256", name: "amountInMax", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapTokensForExactETH",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountOut", type: "uint256" },
+      { internalType: "uint256", name: "amountInMax", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+    ],
+    name: "swapTokensForExactTokens",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  { stateMutability: "payable", type: "receive" },
+];
+
+const provider = new ethers.providers.AlchemyProvider(
+  "homestead",
+  ALCHEMY_API_KEY
+);
+const uniswapV2RouterAddr = ethers.utils.getAddress(UNISWAP_V2_ROUTER_ADDR);
+const uniswapV2RouterInterface = new Interface(UNISWAP_V2_ROUTER_ABI);
+
+async function getTransactionStatus(transactionHash) {
+  const receipt = await provider.getTransactionReceipt(transactionHash);
+  return receipt.status ? "Success" : "Failed";
+}
+
+async function handleTransaction(transaction) {
+  try {
+    const decodedInput = uniswapV2RouterInterface.parseTransaction(transaction);
+    const funcName = decodedInput.name;
+    const args = decodedInput.args;
+
+    const swapFunctions = [
+      "swapExactETHForTokens",
+      "swapExactETHForTokensSupportingFeeOnTransferTokens",
+      "swapExactTokensForETH",
+      "swapExactTokensForETHSupportingFeeOnTransferTokens",
+      "swapExactTokensForTokens",
+      "swapExactTokensForTokensSupportingFeeOnTransferTokens",
+      "swapTokensForExactETH",
+      "swapTokensForExactTokens",
+    ];
+
+    if (swapFunctions.includes(funcName)) {
+      const status = await getTransactionStatus(transaction.hash);
+      const tokens = getTokensInvolved(funcName, args);
+
+      console.log("Transaction Hash:", transaction.hash);
+      console.log("Status:", status);
+      console.log("Function:", funcName);
+      console.log("ERC-20 Tokens Transferred:", tokens);
+    }
+  } catch (error) {
+    console.warn("Unable to decode transaction input data:", error);
+  }
+}
+
+function getTokensInvolved(funcName, args) {
+  if (
+    funcName === "swapExactETHForTokens" ||
+    funcName === "swapExactETHForTokensSupportingFeeOnTransferTokens"
+  ) {
+    return [args.path[0], args.path[args.path.length - 1]];
+  }
+
+  if (
+    funcName === "swapExactTokensForETH" ||
+    funcName === "swapExactTokensForETHSupportingFeeOnTransferTokens" ||
+    funcName === "swapExactTokensForTokens" ||
+    funcName === "swapExactTokensForTokensSupportingFeeOnTransferTokens"
+  ) {
+    return [args.path[0], args.path[args.path.length - 1]];
+  }
+
+  if (
+    funcName === "swapTokensForExactETH" ||
+    funcName === "swapTokensForExactTokens"
+  ) {
+    return [args.path[0], args.path[args.path.length - 1]];
+  }
+
+  return [];
+}
+
+async function monitorTransactions() {
+  let latestBlock = await provider.getBlockNumber();
+
+  while (true) {
+    const currentBlock = await provider.getBlockNumber();
+
+    for (
+      let blockNumber = latestBlock;
+      blockNumber <= currentBlock;
+      blockNumber++
+    ) {
+      const block = await provider.getBlockWithTransactions(blockNumber);
+
+      for (const transaction of block.transactions) {
+        if (
+          transaction.to &&
+          uniswapV2RouterAddr === ethers.utils.getAddress(transaction.to)
+        ) {
+          await handleTransaction(transaction);
+        }
+      }
+    }
+
+    latestBlock = currentBlock;
+  }
+}
+
+monitorTransactions().catch((error) => {
+  console.error("Error monitoring transactions:", error);
+});
